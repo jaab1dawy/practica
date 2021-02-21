ab38883971a1a2760fb02b358b96b07e4de5bf35
Jose Angel Alfonso <joseangelal@gmail.com>
2021-02-21 01:01:12 +0100

    Cambio de Control añadiendo Numeric upDown

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -30,9 +30,10 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.aluNombre = new System.Windows.Forms.TextBox();
-            this.aluNota = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.listaAlumnos = new System.Windows.Forms.TextBox();
+            this.aluNota = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).BeginInit();
             this.SuspendLayout();
             // 
             // aluNombre
@@ -41,13 +42,6 @@ namespace WindowsFormsApp1
             this.aluNombre.Name = "aluNombre";
             this.aluNombre.Size = new System.Drawing.Size(404, 22);
             this.aluNombre.TabIndex = 0;
-            // 
-            // aluNota
-            // 
-            this.aluNota.Location = new System.Drawing.Point(477, 47);
-            this.aluNota.Name = "aluNota";
-            this.aluNota.Size = new System.Drawing.Size(128, 22);
-            this.aluNota.TabIndex = 1;
             // 
             // button1
             // 
@@ -68,17 +62,25 @@ namespace WindowsFormsApp1
             this.listaAlumnos.Size = new System.Drawing.Size(722, 264);
             this.listaAlumnos.TabIndex = 3;
             // 
+            // aluNota
+            // 
+            this.aluNota.Location = new System.Drawing.Point(472, 46);
+            this.aluNota.Name = "aluNota";
+            this.aluNota.Size = new System.Drawing.Size(120, 22);
+            this.aluNota.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.listaAlumnos);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.aluNombre);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,9 +89,9 @@ namespace WindowsFormsApp1
         #endregion
 
         private System.Windows.Forms.TextBox aluNombre;
-        private System.Windows.Forms.TextBox aluNota;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox listaAlumnos;
+        private System.Windows.Forms.NumericUpDown aluNota;
     }
 }
 
